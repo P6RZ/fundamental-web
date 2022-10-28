f8fc423d2b28a1942acce08ce44fda42fe42f8f8
P6RZ <pangeranmlg278@gmail.com>
2022-10-28 19:51:54 +0700

    var string test.go

---
+++ no4_51422279.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string
+	var y string
+	x = "Pangeran"
+	y = "Maharesi Dunia"
+	fmt.Println("My First name: ", x)
+	fmt.Println("My Last name : ", y)
+}
